2984ec45d660cfdf72c491be02643b9282a49dff
matteoliotti <matteo260403@gmail.com>
2025-11-15 14:03:18 +0100

    parte 1
    Variabili e Tipi di Dati

---
+++ progetto2.py
@@ -0,0 +1,22 @@
+#parte 1
+
+nome_M='Mario'
+cognome_M='Rossi'
+eta_M=45
+CodiceFiscale_M='RSSMRA80H12G337L'
+peso_M=78.5
+analisi_M=['emocromo','glicemia','colesterolo']
+
+nome_L='Luigi'
+cognome_L='Rossi'
+eta_L=40
+CodiceFiscale_L='RSSLGI85M23G337T'
+peso_L=82
+analisi_L=['ECG','Antigene Prostatico Specifico','colesterolo']
+
+nome_C='Crisina'
+cognome_C='Bianchi'
+eta_C=22
+CodiceFiscale_C='BNCCRS03R71F205A'
+peso_C=60.5
+analisi_C=['emocromo','Pap test','test della ferritina','test della vitamina D']
